e7fb9453a6ae0d67007d831b54c32e6cda286837
jithuraj <jithuraj@techfriar.com>
2023-06-21 09:44:40 +0530

    initial commit

---
+++ lib/chip_dropdown.dart
@@ -0,0 +1,11 @@
+library chip_dropdown;
+
+import 'package:flutter/widgets.dart';
+class Jithuraj extends StatelessWidget {
+  const Jithuraj({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('aaa');
+  }
+}
